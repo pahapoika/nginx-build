0f0c0c18591f36cdeb8000cb6cea1394396e22da
pahapoika <pahapoika@riseup.net>
2018-03-24 14:02:31 +0200

    Update libressl to 2.7.1

--- build-nginx.sh
+++ build-nginx.sh
@@ -8,14 +8,14 @@ set -e -x
 # Set names of latest versions of each package
 export VERSION_PCRE=pcre-8.41
 export VERSION_ZLIB=zlib-1.2.11
-export VERSION_LIBRESSL=libressl-2.7.0
+export VERSION_LIBRESSL=libressl-2.7.1
 export VERSION_NGINX=nginx-1.13.10
 
 # Set checksums of latest versions
 export SHA256_PCRE=244838e1f1d14f7e2fa7681b857b3a8566b74215f28133f14a8f5e59241b682c
 export SHA256_ZLIB=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
 export SHA256_NGINX=336182104d90be3c40c874f7f06f87dbb357da1dc74ea573ad081a0f29a94885
-export SHA256_LIBRESSL=50ce6d6f88dea73a3efca62b0a9e6ca75292bdee6c9293efd6a771cfdb28cdee
+export SHA256_LIBRESSL=937507e236ba7370acaba689444515d4e8f429136f4e7e3623083392d96b7070
 
 # Set GPG keys used to sign downloads
 export GPG_NGINX=B0F4253373F8F6F510D42178520A9993A1C052F8
